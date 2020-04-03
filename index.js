e9471d4a865e09b749b1876f2401fcae3152fea8
ravi yadav <raviyadav00063@gmail.com>
2020-04-04 02:03:06 +0530

    Modified

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
+const slugify = require('slugify');
+
+const replaceTemplate = require('./modules/replaceTemplate');
+
 //Blocking, synchronous way
 // const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
 // console.log(textIn);
@@ -28,19 +32,46 @@ const url = require('url');
 // console.log('will read file');
 
 //SERVER
+
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
-const server = http.createServer((req, res) => {
-    const pathName = req.url;
+const slugs = dataObj.map(el => slugify(el.productName, { lower: true }));
+console.log(slugs);
 
-    if(pathName === '/' || pathName === '/overview') {
-        res.end('This is the OVERVIEW');
-    } else if (pathName === '/product') {
-        res.end('This is the PRODUCT');
-    } else if (pathName === '/api') {  
+const server = http.createServer((req, res) => {
+
+   const { query, pathname } = url.parse(req.url, true);
+    
+    //OVERVIEW PAGE
+    if(pathname === '/' || pathname === '/overview') {
+        res.writeHead(200, { 'Content-type': 'text/html'});
+
+        const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+        const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+
+        
+        res.end(output);
+
+    //PRODUCT PAGE    
+    } else if (pathname === '/product') {
+        res.writeHead(200, { 'Content-type': 'text/html'});
+        const product = dataObj[query.id];
+        const output = replaceTemplate(tempProduct, product);
+
+
+        res.end(output);
+
+    //API    
+    } else if (pathname === '/api') {  
      res.writeHead(200, { 'Content-type': 'application/json'});
      res.end(data);
+
+    //NOT FOUND 
     } else {
       res.writeHead(404, {
           'content-type': 'text/html',
